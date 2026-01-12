36b4b1615be544c2b12f98348a66875f915ac0e5
emreerdener <erdener.emre@gmail.com>
2026-01-12 13:57:18 -0600

    Footer updates

--- src/features/layout/components/Footer.tsx
+++ src/features/layout/components/Footer.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ActionIcon, Group, Stack, Text } from "@mantine/core";
+import { ActionIcon, Card, Flex, Grid, Stack, Text } from "@mantine/core";
 import {
   IconBrandFacebook,
   IconBrandInstagram,
@@ -16,47 +16,69 @@ const ThemeSwitch = dynamic(
 
 export default function Footer() {
   return (
-    <Stack p="lg">
-      <Group justify="space-between" h="100%">
-        <Group gap="xs">
-          <ActionIcon
-            variant="subtle"
-            aria-label="Apparel Manufacturing Facebook"
-            size="lg"
-          >
-            <IconBrandFacebook size={24} />
-          </ActionIcon>
+    <Stack gap={0}>
+      <Card p="lg" radius={0}>
+        Footer content
+      </Card>
 
-          <ActionIcon
-            variant="subtle"
-            aria-label="Apparel Manufacturing X"
-            size="lg"
-          >
-            <IconBrandX size={24} />
-          </ActionIcon>
+      {/* Sub Footer */}
+      <Grid h="100%" p="lg" justify="center" align="center" gutter="lg">
+        {/* Social icons */}
+        <Grid.Col span={{ base: 12, sm: 4 }} order={{ base: 2, sm: 1 }}>
+          <Flex justify={{ base: "center", sm: "flex-start" }} gap="xs">
+            <ActionIcon
+              variant="subtle"
+              aria-label="Apparel Manufacturing Facebook"
+              size="lg"
+            >
+              <IconBrandFacebook size={24} />
+            </ActionIcon>
 
-          <ActionIcon
-            variant="subtle"
-            aria-label="Apparel Manufacturing LinkedIn"
-            size="lg"
-          >
-            <IconBrandLinkedin size={24} />
-          </ActionIcon>
+            <ActionIcon
+              variant="subtle"
+              aria-label="Apparel Manufacturing X"
+              size="lg"
+            >
+              <IconBrandX size={24} />
+            </ActionIcon>
 
-          <ActionIcon
-            variant="subtle"
-            aria-label="Apparel Manufacturing Instagram"
-            size="lg"
-          >
-            <IconBrandInstagram size={24} />
-          </ActionIcon>
-        </Group>
+            <ActionIcon
+              variant="subtle"
+              aria-label="Apparel Manufacturing LinkedIn"
+              size="lg"
+            >
+              <IconBrandLinkedin size={24} />
+            </ActionIcon>
 
-        <Text size="sm" c="dimmed">
-          © 2026 Apparel Manufacturing Co., Inc.
-        </Text>
-        <ThemeSwitch />
-      </Group>
+            <ActionIcon
+              variant="subtle"
+              aria-label="Apparel Manufacturing Instagram"
+              size="lg"
+            >
+              <IconBrandInstagram size={24} />
+            </ActionIcon>
+          </Flex>
+        </Grid.Col>
+
+        {/* Copyright */}
+        <Grid.Col span={{ base: 12, sm: 4 }} order={{ base: 3, sm: 2 }}>
+          <Text
+            size="sm"
+            c="dimmed"
+            ta="center"
+            style={{ whiteSpace: "nowrap" }}
+          >
+            © 2026 Apparel Manufacturing Co., Inc.
+          </Text>
+        </Grid.Col>
+
+        {/* Mode switch */}
+        <Grid.Col span={{ base: 12, sm: 4 }} order={{ base: 1, sm: 3 }}>
+          <Flex justify={{ base: "center", sm: "flex-end" }}>
+            <ThemeSwitch />
+          </Flex>
+        </Grid.Col>
+      </Grid>
     </Stack>
   );
 }
